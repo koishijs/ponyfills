c60be032dfa6ed6417582dc8ef052bee927795af
Shigma <shigma10826@gmail.com>
2023-07-05 01:22:14 +0800

    feat(fs): init watch API

--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,5 +1,7 @@
 import * as constants from './constants'
 import * as promises from './promises'
+import EventEmitter from 'events'
+import { Matcher } from 'anymatch'
 
 export { constants, promises }
 
@@ -17,4 +19,24 @@ export class Dirent {
   isDirectory() {
     return this.handle.kind === 'directory'
   }
+}
+
+export interface WatchOptions {
+  ignored?: Matcher
+  ignoreInitial?: boolean
+  cwd?: string
+  depth?: number
+  interval?: number
+}
+
+export class FSWatcher extends EventEmitter {
+  constructor(paths: string | readonly string[], options?: WatchOptions) {
+    super()
+  }
+
+  close() {}
+}
+
+export function watch(paths: string | readonly string[], options?: WatchOptions) {
+  return new FSWatcher(paths, options)
 }
